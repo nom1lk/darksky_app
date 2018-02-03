8114687c32e09f712c1fc232d4c6bee0083e7c8f
Steve Condylios <steve.condylios@gmail.com>
2018-02-04 02:30:10 +1100

    Added resorts array of arrays - backing up before converting to hash with coordinates array as values

--- darksky1/app/controllers/application_controller.rb
+++ darksky1/app/controllers/application_controller.rb
@@ -11,6 +11,56 @@ require 'nokogiri'
 require 'open-uri'
 
 
+
+resorts = [
+["Niseko", 42.7925607,140.6145884],
+["Rusutsu", 42.720765,140.8246208],
+["Furano", 43.2788929,142.3346243],
+["Kiroro", 43.1015532,143.5976088],
+
+["Hakuba", 36.6908076,137.7758388],
+["Nozawa Onsen", 36.9332568,138.4344672],
+["Shiga Kogen", 36.7052466,138.5058287],
+["Myoko Kogen", 36.892743,138.1740073],
+
+["YongPyong", 37.6429587,128.6781007],
+["Alpensia", 37.6542186,128.6695522],
+["Phoenix Snow Park", 37.5812246,128.3251215],
+["High1 Resort", 37.2049058,128.8375798],
+["Vivaldi Park", 37.648144,127.6819313],
+
+["Yabuli", 44.780865,128.4454603],
+["Beidahu", 43.422146,126.6160153], #took ski field
+["Changbaishan", 41.7446585,127.6853884],
+["Chongli", 41.0354696,114.9045811],
+]
+
+
+
+##### DWD - WORKS
+for each in resorts 
+puts each[0]
+end
+
+
+
+
+######
+
+
+
+for each in resorts
+
+"#{each[0]}" + "_url" = "https://api.darksky.net/forecast/979b169b4243ddb0a2ea22801e966bd0/" + each[1].to_s + "," + each[2].to_s
+
+end
+
+
+
+
+
+
+# niseko
 url = "https://api.darksky.net/forecast/979b169b4243ddb0a2ea22801e966bd0/42.7925607,140.6145884"
 doc = Nokogiri::HTML(open(url))
 data = JSON.parse(doc)
@@ -20,6 +70,19 @@ data = JSON.parse(doc)
 @three_day = (data["daily"]["data"][1]["precipAccumulation"] + data["daily"]["data"][2]["precipAccumulation"] + data["daily"]["data"][3]["precipAccumulation"]).round(1) 
 #seven_day = data["daily"]["data"][0]["precipAccumulation"] + data["daily"]["data"][1]["precipAccumulation"] + data["daily"]["data"][2]["precipAccumulation"] + data["daily"]["data"][3]["precipAccumulation"] + data["daily"]["data"][4]["precipAccumulation"] + data["daily"]["data"][5]["precipAccumulation"] + data["daily"]["data"][6]["precipAccumulation"]
 @seven_day = (data["daily"]["data"][1]["precipAccumulation"] + data["daily"]["data"][2]["precipAccumulation"] + data["daily"]["data"][3]["precipAccumulation"] + data["daily"]["data"][4]["precipAccumulation"] + data["daily"]["data"][5]["precipAccumulation"] + data["daily"]["data"][6]["precipAccumulation"] + data["daily"]["data"][7]["precipAccumulation"]).round(1)
+
+
+
+
+
+
+
+"#{a}" + "banana"
+
+
+
+
+
 
 
 render template: 'scrape_darksky'
