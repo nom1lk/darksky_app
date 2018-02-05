28987663e1ef876ee854d804b864495bbb08a8d3
Steve Condylios <steve.condylios@gmail.com>
2018-02-05 15:49:33 +1100

    Retry

--- darksky1/config/application.rb
+++ darksky1/config/application.rb
@@ -11,16 +11,18 @@ module Darksky1
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+}
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 
-config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOWALL'
-}
 
-  
+
+
 end
 
 
